892ae9ed3d62acba1f41c2ba12077e30e8379eef
KTalagery <karantalagery@gmail.com>
2025-05-15 04:00:59 -0500

    music

--- Assets/GawkerBehaviour.cs
+++ Assets/GawkerBehaviour.cs
@@ -5,19 +5,30 @@ public class GawkerBehaviour : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public int _intensity;
     public int BaseGlow;
+    public AudioSource[] a;
     public int Intensity
     {
         get { return _intensity; }
         set
         {
-            _intensity = value + BaseGlow;
-            GetComponent<Light>().intensity = _intensity * 1000;
+            _intensity = value;
+            GetComponent<Light>().intensity = (_intensity + BaseGlow)* 1000;
+            songChange(value);
         }
     }
 
     public void setIntensity(int intensity)
     {
         Intensity = intensity;
+    }
+
+    public void songChange(int i)
+    {
+        foreach (AudioSource source in a)
+        {
+            source.volume = 0;
+        }
+        a[i].volume = 1;
     }
 
 }
